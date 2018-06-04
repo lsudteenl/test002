3fd773415d444916711c95b5a9d8fcb6e649acba
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-06-04 17:19:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-{
-    "type": "text",
-    "text": "\uDBC0\uDC84 LINE emoji"
-}
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?> 
+
+</body>
+</html>
