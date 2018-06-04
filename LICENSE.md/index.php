146b5dc70f56d4f45caf3bf8449f9128121da70d
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-06-04 17:07:22 +0700

    Delete index.php

--- LICENSE.md/index.php
+++
@@ -1,7 +0,0 @@
-{
-  "line": {
-    "type": "image",
-    "originalContentUrl": "https://www.picz.in.th/images/2018/06/04/zyOBnq.jpg",
-    "previewImageUrl": "https://www.picz.in.th/images/2018/06/04/zyOBnq.jpg"
-  }
-}
