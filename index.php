19f07efbd6c177444c327cb5c85c3829e018b36f
lsudteenl <39257650+lsudteenl@users.noreply.github.com>
2018-06-04 17:16:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<cVEzC6zHrhzVc5gHgNwQ4LhEi/4w+uGMqTaJEtYFWNmtiobUlcMOkab5Ay2sHA7OXxxozIhjQ1h5ikQkVUQHZv4eld7MGeUR7Vu++UvHRZB7s0ykSwwLjf5ilqwyLPej9SPZk3s3bfyQzHEqmwlcSAdB04t89/1O/w1cDnyilFU=
->');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<5532695f618d2202f4b24527c9bf74cb>']);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<cVEzC6zHrhzVc5gHgNwQ4LhEi/4w+uGMqTaJEtYFWNmtiobUlcMOkab5Ay2sHA7OXxxozIhjQ1h5ikQkVUQHZv4eld7MGeUR7Vu++UvHRZB7s0ykSwwLjf5ilqwyLPej9SPZk3s3bfyQzHEqmwlcSAdB04t89/1O/w1cDnyilFU=
->', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+{
+    "type": "text",
+    "text": "\uDBC0\uDC84 LINE emoji"
+}
